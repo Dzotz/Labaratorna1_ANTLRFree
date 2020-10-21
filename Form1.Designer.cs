11061ab06cf1f85b91b2305e0731fea8a2c9ac28
Dzotz <danilo.zotov@gmail.com>
2020-10-22 00:29:32 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Labaratorna1_ANTLRFree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button6);
+            this.splitContainer1.Panel1.Controls.Add(this.button5);
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(852, 324);
+            this.splitContainer1.SplitterDistance = 41;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(380, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(288, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Delete Row";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(638, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(741, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(196, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete Column";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add Row";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Column";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(852, 279);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 324);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+
+namespace Labaratorna1_ANTLRFree
+{
+    public partial class Form1 : Form
+    {
+
+        //string columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //string rows = "1234567890";
+        int curRow, curCol;
+        public Dictionary<string, Cell> dict = new Dictionary<string, Cell>();
+        public Dictionary<string, string> dict2 = new Dictionary<string, string>();
+        public Dictionary<string, string> dict3 = new Dictionary<string, string>();
+        Parser pars = new Parser();
+        void DGVInitializer()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Cell cell = new Cell();
+                DataGridViewColumn column = new DataGridViewColumn(cell);
+                DataGridViewRow row = new DataGridViewRow();
+                row.HeaderCell.Value = (i+1).ToString();
+                column.HeaderText = Converter26.To26(i);
+                column.Name = Converter26.To26(i);
+                dataGridView1.Columns.Add(column);
+                dataGridView1.Rows.Add(row);
+                
+            }
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    string cell_name = Converter26.To26(j) + (i + 1).ToString();
+                    Cell cell = new Cell();
+                    cell.Value = "0";
+                    cell.Exp = "0";
+                    dict.Add(cell_name, cell);
+                    dict2.Add(cell_name, "");
+                    dict3.Add(cell_name, "");
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            DGVInitializer();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+   
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            try
+            {
+                curRow = dataGridView1.CurrentCell.RowIndex;
+                curCol = dataGridView1.CurrentCell.ColumnIndex;
+                string cell_name = Converter26.To26(curCol) + (curRow + 1).ToString();
+                dataGridView1[curCol, curRow].Value = dict[cell_name].Exp;
+            }
+            catch { }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                curRow = dataGridView1.CurrentCell.RowIndex;
+                curCol = dataGridView1.CurrentCell.ColumnIndex;
+                string cell_name = Converter26.To26(curCol) + (curRow + 1).ToString();
+                string str = dataGridView1[curCol, curRow].Value.ToString();
+                dict2[cell_name] = str;
+                dict[cell_name].Exp = str;
+                string st = AdressAnalizator(str, cell_name);
+                dict3[cell_name] = st;
+
+                if (IsRecur(cell_name)){
+                    MessageBox.Show("Expression is recur");
+                    dataGridView1[curCol, curRow].Value = "0";
+                    dict[cell_name].Value = "0";
+                    dict[cell_name].Exp = "0";
+                    dict[cell_name].Depends.Clear();
+                    dict[cell_name].Depends.Capacity=0;
+                    dict2[cell_name] = "";
+                    dict3[cell_name] = "";
+                }
+                else
+                {
+                    
+                    double res = pars.ExpressionStart(st);
+                    string result = res.ToString();
+                    string errors = pars.err;
+                    if (errors != "")
+                    {
+                        if (errors == "no expression" || errors == "syntax error")
+                        {
+                            dataGridView1[curCol, curRow].Value = dict2[cell_name];
+                            dict[cell_name].Value = dict2[cell_name];
+                        }
+                        else
+                        {
+                            dataGridView1[curCol, curRow].Value = "0";
+                            dict[cell_name].Value = "0";
+                            dict[cell_name].Exp = "0";
+                            dict[cell_name].Depends.Clear();
+                            dict[cell_name].Depends.Capacity = 0;
+                            dict2[cell_name] = "";
+                            dict3[cell_name] = "";
+                        }
+                        pars.err = "";
+                    }
+                    else
+                    {
+                        dict[cell_name].Value = result;
+                        dataGridView1[curCol, curRow].Value = dict[cell_name].Value;
+                    }
+                }
+                RefreshCells();
+            }
+            catch { }
+        }
+
+        public string AdressAnalizator(string str, string cell_name)
+        {
+            string st = "";
+            char[] delim = { ' ' };
+            List<string> lex = new List<string>(str.Split(delim));
+            dict[cell_name].Depends.Clear();
+            dict[cell_name].Depends.Capacity = 0;
+            for (int i = 0; i<lex.Count;i++)
+            {
+                string lexem = lex[i];
+                if (dict.ContainsKey(lexem))
+                {
+                    dict[cell_name].Depends.Add(lexem);
+                    lexem = dict[lexem].Value;
+
+                    lex[i] = lexem;
+                }
+            }
+            for(int i = 0; i<lex.Count; i++)
+            {
+                st += lex[i];
+            }
+            str = st;
+            return str;
+        }
+
+
+        bool dop_rec(string cell_name1, string cell_name2)
+        {
+            if (dict[cell_name1].Depends.Contains(cell_name2))
+            {
+                return true;
+            }
+            else
+            {
+                List<string> dep = dict[cell_name1].Depends;
+                for (int i=0; i<dep.Count; i++)
+                {
+                    return dop_rec(dep[i], cell_name1);
+                }
+            }
+            return false;
+        }
+
+        public bool IsRecur(string cell_name)
+        {
+            return dop_rec(cell_name, cell_name);
+        }
+
+        public void RefreshCells()
+        {
+            for(int i = 0; i< dataGridView1.ColumnCount; i++)
+            {
+                for (int j=0; j<dataGridView1.RowCount-1; j++)
+                {
+                    string cell_name = Converter26.To26(i) + (j + 1).ToString();
+                    if (dict.ContainsKey(cell_name) == false)
+                    {
+                        Cell cell = new Cell();
+                        cell.Value = "0";
+                        cell.Exp = "0";
+                        dict.Add(cell_name, cell);
+                        dict2.Add(cell_name, "");
+                        dict3.Add(cell_name, "");
+                    }
+                    string st = AdressAnalizator(dict[cell_name].Exp, cell_name);
+                    string res = pars.ExpressionStart(st).ToString();
+                    dict[cell_name].Value = res;
+                    dataGridView1[i, j].Value = dict[cell_name].Value;
+                }
+            }
+        }
+
+        void AddRow()
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.HeaderCell.Value = (dataGridView1.RowCount).ToString();
+            dataGridView1.Rows.Add(row);
+            RefreshCells();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AddRow();
+        }
+
+        void AddColumn()
+        {
+            DataGridViewColumn column = new DataGridViewColumn();
+            DataGridViewCell cell = new DataGridViewTextBoxCell();
+            column.CellTemplate = cell;
+            column.HeaderText = Converter26.To26(dataGridView1.ColumnCount);
+            column.Name = Converter26.To26(dataGridView1.ColumnCount);
+            dataGridView1.Columns.Add(column);
+            RefreshCells();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddColumn();
+        }
+
+
+        void DeletedCellUpd(int row, int col)
+        {
+            string cell_name = Converter26.To26(col) + (row + 1).ToString();
+            dict2[cell_name] = "";
+            dict[cell_name].Value = "0";
+            dict[cell_name].Exp = "0";
+            dict3[cell_name] = "";
+        }
+
+        void DelRow()
+        {
+            try
+            {
+                int row = dataGridView1.RowCount - 2;
+                for (int col = 0; col < dataGridView1.ColumnCount; col++)
+                {
+                    DeletedCellUpd(row, col);
+                }
+                RefreshCells();
+                dataGridView1.Rows.RemoveAt(row);
+
+            }
+            catch { }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DelRow();
+        }
+
+        void DelColumn()
+        {
+            try
+            {
+                int col = dataGridView1.ColumnCount - 1;
+                for (int row = 0; row<dataGridView1.RowCount-1; row++)
+                {
+                    DeletedCellUpd(row, col);
+                }
+                RefreshCells();
+                dataGridView1.Columns.RemoveAt(col);
+            }
+            catch { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DelColumn();
+        }
+
+        void SaveFile()
+        {
+
+        }
+
+        void OpenFile()
+        {
+
+        }
+    }
+}
